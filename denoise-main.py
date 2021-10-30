e7b43d445077fbc4724f37bb6e45c21ed8a26a9d
Anil Kumar Mysore Badarinarayana <anil_kumar.mysore_badarinarayana@mailbox.tu-dresden.de>
2021-10-30 17:03:47 +0200

    code cleaning

---
+++ denoise-main.py
@@ -0,0 +1,198 @@
+from operator import le
+import matplotlib.pyplot as plt
+import numpy as np
+from time import time
+# import cProfile
+# import io
+# import pstats
+import multiprocessing as mp
+from multiprocessing import freeze_support
+from random import randint
+import torch
+
+from src import helperfuncs
+from src_parallel import classify_parallel
+from src_conv import classify_conv
+from src import classify
+from src import cluster
+
+
+def main():
+
+    # folderPath = 'C:/My Documents/TUD-MCL/Semester 4/Thesis/Implementation/Data/Dataset-4/NMC111_delith_15000000X_ABF_stack2/' # Maxime/' #sample 2/'
+    folderPath = 'C:/My Documents/TUD-MCL/Semester 4/Thesis/Implementation/Data/Dataset-1/'
+    # imgName = 'NMC111_delith_15000000X_ABF_stack2.dm3'
+    imgName = '18_04_27_Thomas_28618_0017.dm3'
+    # imgName = 'Stack_zeolite4NaAF__111_001_1-10.tif'
+    rerun = 15
+    radius = 23
+    clusteringFactor = 2.71
+    analyze = False
+
+# def denoise(folderPath, imgName, rerun = 15, radius=23):
+
+    start = time()
+
+    
+
+    load_start = time()
+    imgs = helperfuncs.loadData(folderPath=folderPath, fileName=imgName)
+    load_end = time()
+    print(f'Time for loading: {load_end - load_start} seconds!')
+
+    startPosList = []
+    noOfInititalPatches = np.max(np.asarray(imgs[0].shape)//100)
+    if noOfInititalPatches<2:
+        noOfInititalPatches = 2
+    for i in range(noOfInititalPatches):
+        startPosList.append([randint(0,imgs[0].shape[0]-radius), randint(0,imgs[0].shape[0]-radius)])
+
+    # startPosList= [[84-radius,404-radius],[97-radius,404-radius],[88-radius,404-radius]]
+
+    NumMainclasses=4
+    MinNumberInClass=4
+    MaxNumberInClass=100*int(np.ceil(np.sqrt(len(imgs))))
+    # MaxNumberInClass=100
+
+    if analyze:
+        n1_max=1
+        n1=0
+        n2_max=len(imgs)
+        n2=0
+        plt.figure(figsize=(20, 20*n2_max/n1_max)) 
+        for img in imgs:
+            n2+=1    
+            vstd=np.std(img)
+            vmean=np.mean(img)         
+            ax1=plt.subplot(n2_max,n1_max,n1*n2_max+n2)
+            ax1.imshow(img,cmap='gray',vmin=np.min(img),vmax=np.max(img))
+            ax1.axis('off')
+            
+        plt.show()
+
+    gen_start = time()
+    templates = helperfuncs.generateTemplates(startPosList=startPosList, imgs=imgs, radius=radius)
+    # templates = helperfuncs.findDissimilarTemplates(templates = templates, imgs = imgs, radius = radius, minTemplateClasses = NumMainclasses)
+    gen_end = time()
+    print(f'Time for generating basic templates: {gen_end - gen_start} seconds!')
+
+    rerun_ = rerun
+    classsify_start = time()
+    templatesCount = []
+
+    if len(imgs)>1:
+
+        if torch.cuda.is_available():
+            while rerun>0:
+                templates = classify_conv.tempfuncname(radius=radius, imgs=imgs, templates=templates, maxNumberInClass=MaxNumberInClass, minNumberInClass=MinNumberInClass)
+                if(len(templatesCount)!=0):
+                    if(templatesCount[-1]==len(templates)):
+                        if(templatesCount[-1]==templatesCount[-2]):
+                            break
+                templatesCount.append(len(templates))
+                print(f'Completed iteration')
+                rerun-=1
+            backplot, min, max, templateMatchingResults = classify_conv.backplotImg(radius, imgs, templates)
+            
+        else:
+            pool = mp.Pool(mp.cpu_count())
+            while rerun>0:
+                templates = classify_parallel.tempfuncname(radius=radius, imgs=imgs, templates=templates, maxNumberInClass=MaxNumberInClass, minNumberInClass=MinNumberInClass, pool= pool)                
+                if(len(templatesCount)!=0):
+                    if(templatesCount[-1]==len(templates)):
+                        if(templatesCount[-1]==templatesCount[-2]):
+                            break
+                templatesCount.append(len(templates))
+                print(f'Completed iteration')
+                rerun-=1
+            backplot, min, max, templateMatchingResults = classify_parallel.backplotImg(radius, imgs, templates, pool)
+            pool.close()
+
+    else:
+        while rerun>0:
+            templates = classify_conv.tempfuncname(radius=radius, imgs=imgs, templates=templates, maxNumberInClass=MaxNumberInClass, minNumberInClass=MinNumberInClass)
+            if(len(templatesCount)!=0):
+                if(templatesCount[-1]==len(templates)):
+                    if(templatesCount[-1]==templatesCount[-2]):
+                        break
+            templatesCount.append(len(templates))
+            print(f'Completed iteration')
+            rerun-=1
+        backplot, min, max, templateMatchingResults = classify_conv.backplotImg(radius, imgs, templates)
+        
+
+    classify_end = time()
+    print(f'Time for generating extra templates and classifying {rerun_} times: {classify_end - classsify_start} seconds!')
+    
+    sort_start = time()
+    picDic = cluster.sortTemplates(imgs, templateMatchingResults, radius, templates)
+    sort_end = time()
+    print(f'Time for sort : {sort_end - sort_start} seconds!')
+
+    cluster_start = time()
+    centroidDic = cluster.cluster(radius, templates, picDic, clusteringFactor)
+    cluster_end = time()
+    print(f'Time for clustering : {cluster_end - cluster_start} seconds!')
+
+    backplot_start = time()
+    backplotFinal, min, max = cluster.backplotFinal(centroidDic, picDic, imgs, radius, templateMatchingResults)    
+    backplot_end = time()
+    print(f'Time for backplotting-2 : {backplot_end - backplot_start} seconds!')
+
+    if analyze:
+        for i in range(len(imgs)):
+            plt.figure(figsize=(2*15, 2*7)) 
+            ax1=plt.subplot(1,2,1)                    
+            ax1.imshow(imgs[i][radius:-radius,radius:-radius],cmap=plt.cm.gray,vmin=0,vmax=512)
+            ax1.set_title('original image')
+            ax1.axis('off')
+            ax2=plt.subplot(1,2,2)                    
+            ax2.imshow(backplotFinal[i][radius:-radius,radius:-radius],cmap=plt.cm.gray,vmin=0,vmax=512)
+            ax2.set_title('backplot')
+            ax2.axis('off')
+            #plt.figure(figsize=(15, 12))  
+            #plt.imshow(overlayclass[Mode][myindex],cmap=plt.cm.gist_rainbow)
+            #plt.colorbar()
+            # plt.show()
+         
+        plt.savefig('C:/My Documents/TUD-MCL/Semester 4/Thesis/repo/img-denoiser/results/parallel-stack-'+imgName+'-denoised.png')    
+
+    if analyze:
+        for i in range(len(imgs)):
+            plt.figure(figsize=(20,20))
+            img = np.log(np.abs(np.fft.fftshift(np.fft.fft2(imgs[i][radius:-radius,radius:-radius]))))
+            ax1=plt.subplot(1,2,1)
+            ax1.imshow(img,cmap='gray')
+            ax1.axis('off')
+            ax1.set_title('FFT of original image')
+            img = np.log(np.abs(np.fft.fftshift(np.fft.fft2(backplotFinal[i][radius:-radius,radius:-radius]))))
+            ax1=plt.subplot(1,2,2)
+            ax1.imshow(img,cmap='gray')
+            ax1.axis('off')
+            ax1.set_title('FFT of denoised image in')
+            plt.show()
+
+    end = time()
+    print(f'Total time: {end - start} seconds!')
+
+    return backplotFinal
+
+
+
+# denoise(folderPath, imgName, rerun = 15, radius=23)
+# cProfile.run('denoise(folderPath, imgName, rerun = 15, radius=23)')
+
+# def main():
+# pr = cProfile.Profile()
+# pr.enable()
+# denoise(folderPath, imgName, rerun = 15, radius=23)
+# pr.disable()
+# s = io.StringIO()
+# sortby = 'cumulative'
+# ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+# ps.print_stats("denoise")
+# print(s.getvalue())
+
+if __name__ == '__main__':
+    freeze_support()
+    main()
