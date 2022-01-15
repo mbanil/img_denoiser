2626378a8c71d066219fd4fc7aaf40c8b18fe805
Anil Kumar Mysore Badarinarayana <anil_kumar.mysore_badarinarayana@mailbox.tu-dresden.de>
2022-01-15 01:49:35 +0100

    cleaned analyse code

--- analyse/time-complexity.py
+++ analyse/time-complexity.py
@@ -1,28 +1,31 @@
 
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-# imgSize = [128,256,512,1024]
-# pixels = [number ** 2 for number in imgSize]
-# # t1 = np.array([0.75,1.895,22.13,283.056])
-# # t2 = np.array([0.79,1.994,25.386,398.365])
-# # t3 = np.array([0.75,2.156,18.336,2.156])
+imgSize = [128,256,512,1024]
+pixels = [number ** 2 for number in imgSize]
+t1 = np.array([0.75,1.895,22.13,283.056])
+t2 = np.array([0.79,1.994,25.386,398.365])
+t3 = np.array([0.75,2.156,18.336,2.156])
 
-# # t = np.add(t1,t2)
-# # t = np.add(t,t3)
-# # t = t/3
+t = np.add(t1,t2)
+t = np.add(t,t3)
+t = t/3
 
-# pixels = np.array([128*128, 256*256,500*600,500*800,625*800,600*1024,800*1024,1024*1024])
+pixels = np.array([128*128, 256*256,500*600,500*800,625*800,600*1024,800*1024,1024*1024])
 
-# t = np.array([0.79,1.99, 25.91,43.12,65.33,103.76,198.67,313.77])
+t = np.array([0.79,1.99, 25.91,43.12,65.33,103.76,198.67,313.77])
   
-# # plotting the points 
-# plt.plot(pixels, t,'-bo')
-# plt.xlabel('No. of pixels')
-# plt.ylabel('Time in secs')
-# plt.title('Time-Complexity')
+# plotting the points 
+plt.plot(pixels, t,'-bo')
+plt.xlabel('No. of pixels')
+plt.ylabel('Time in secs')
+plt.title('Time-Complexity')
 
-# plt.savefig('C:/My Documents/TUD-MCL/Semester 4/Thesis/repo/img-denoiser/analyse/plot.png')    
+plt.savefig('C:/My Documents/TUD-MCL/Semester 4/Thesis/repo/img-denoiser/analyse/plot.png')    
+
+
+# ==========================================================================================
 
 
 import matplotlib.pyplot as plt
@@ -30,16 +33,8 @@ import numpy as np
 
 templateSize = np.array([15, 18, 21, 24, 27, 30])
 templateSize = templateSize*2
-# pixels = [number ** 2 for number in imgSize]
-# t1 = np.array([0.75,1.895,22.13,283.056])
-# t2 = np.array([0.79,1.994,25.386,398.365])
-# t3 = np.array([0.75,2.156,18.336,2.156])
 
-# t = np.add(t1,t2)
-# t = np.add(t,t3)
-# t = t/3
-
-# pixels = np.array([128*128, 256*256,500*600,500*800,625*800,600*1024,800*1024,1024*1024])
+pixels = np.array([128*128, 256*256,500*600,500*800,625*800,600*1024,800*1024,1024*1024])
 
 t = np.array([45.8, 31.00, 25.8, 21.83, 18.53, 16.51])
   
@@ -47,6 +42,5 @@ t = np.array([45.8, 31.00, 25.8, 21.83, 18.53, 16.51])
 plt.plot(templateSize, t,'-bo')
 plt.xlabel('Reference patch size')
 plt.ylabel('Time in secs')
-# plt.title('Time-Complexity')
 
 plt.savefig('C:/My Documents/TUD-MCL/Semester 4/Thesis/repo/img-denoiser/analyse/tempSizevstime.png')   
