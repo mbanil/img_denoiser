1706cbcd4f765e401f9ff5b1a37eb922c5d2a6e1
akmb <anilkumar.mb@deepxscan.com>
2023-08-31 16:57:20 +0200

    bug fixes

--- denoiser/run.py
+++ denoiser/run.py
@@ -7,7 +7,7 @@ import numpy as np
 def run(image_path, result_folder_path, min_patches_per_class, max_patches_per_class, iteration_counter, patch_size, termination_number, analyze, clustering_factor):
 
     image = tifffile.imread(image_path).astype(np.float32)
-    file_name = image_path.split("/")[-1].split(".")[0]
+    file_name = image_path.split("/")[-1].split("\\")[-1].split(".")[0]
 
     if(len(image.shape)==2):
         image = image[np.newaxis,...]
@@ -19,7 +19,7 @@ def run(image_path, result_folder_path, min_patches_per_class, max_patches_per_c
                                   termination_number, 
                                   analyze, 
                                   clustering_factor)
-    if(image.shape[0]==1):
+    elif(image.shape[0]==1):
         denoised_image = denoiser(image, 
                                   min_patches_per_class, 
                                   max_patches_per_class, 
@@ -28,7 +28,7 @@ def run(image_path, result_folder_path, min_patches_per_class, max_patches_per_c
                                   termination_number, 
                                   analyze, 
                                   clustering_factor)
-    if(len(image.shape)==3):
+    elif(len(image.shape)==3):
         denoised_image = np.zeros((image.shape))
         for i in range(image.shape[0]):
             denoised_image[i,:,:] = denoiser(image[i:i+1,:,:], 
@@ -40,7 +40,7 @@ def run(image_path, result_folder_path, min_patches_per_class, max_patches_per_c
                                              analyze, 
                                              clustering_factor)[0]
 
-    tifffile.imsave(result_folder_path + "denoised_" + file_name + ".tif", denoised_image)
+    tifffile.imsave(result_folder_path + "/denoised_" + file_name + ".tif", denoised_image)
 
 if __name__ == "__main__":
 
